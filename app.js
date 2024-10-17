8177f869ca67b2a66127c9bc4ee8d37a73af676c
Ai-Chen-Hsieh <a0903109727@gmail.com>
2024-10-17 10:26:30 +0800

    chore: install & setting package

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const mongoose = require('mongoose')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -11,6 +12,17 @@ const routes = require('./routes')
 require('./config/mongoose')
 
 const app = express()
+mongoose.connect(process.env.MONGODB_URI) //設定連線至mongodb
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 const PORT = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
